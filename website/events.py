8c127623d6d432ab1d2002c8d522d5fc11d0803f
jenmebs <n9721886@qut.edu.au>
2024-10-21 11:34:53 +1000

    Initial changes to events.py
    Initial changes to code to get data from events database after a new event creation/submission, rather than its current hardcoded setup

--- website/events.py
+++ website/events.py
@@ -1,18 +1,36 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Event
 from .forms import CreateEventForm
+from . import db 
 
 event_bp = Blueprint('event', __name__, url_prefix='/events')
 
-@event_bp.route('/<id>')
+@event_bp.route('/<int:id>')  #original line: @event_bp.route('/<id>')
 def show(id):
     event = get_event()
     return render_template('events/event.html', event=event)
 
-@event_bp.route('/create')
+@event_bp.route('/create', methods=['GET', 'POST'])  #original line: @event_bp.route('/create')
 def create():
-  create = CreateEventForm()
-  return render_template('events/create.html', form=create)
+  form = CreateEventForm() #original line: create = CreateEventForm()
+  if (form.validate_on_submit()==True):
+     new_event = Event (
+        title = form.title.data,
+        desc = form.desc.data,
+        imaage = form.image.data.filename,
+        date = form.date.data,
+        month = form.month.data,
+        nightclub = form.nightclub.data,
+        event_type = form.event_type.data,
+        age_range = form.age_range.data
+     )
+
+     db.session.add(new_event)
+     db.session.commit()
+
+     return redirect(url_for('event.show', id=new_event.id))
+  
+  return render_template('events/create.html', form=form) #original line: return render_template('events/create.html', form=create)
 
 def get_event():
   # creating the description of Brazil
