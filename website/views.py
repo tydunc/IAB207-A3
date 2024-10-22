339f74f4c8c6b4e8f2c1b36d3ed7557d494ae8c4
tydunc <n11405741@qut.edu.au>
2024-10-22 19:09:49 +1000

    Updated index page to show all events

--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template
+from .models import Events
+from . import db
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
     background = 'bg-1'
-    return render_template('index.html', background=background)
+    events = db.session.scalars(db.select(Events)).all()
+    return render_template('index.html', background=background, events=events)
 
 @main_bp.route('/bookings')
 def bookings():
